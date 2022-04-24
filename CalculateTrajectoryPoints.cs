53deb38b2843be0f4faed5ef2867ad2a8218cd1d
Piotr <58243036+PhoXinuS@users.noreply.github.com>
2022-04-24 11:11:05 +0200

    Complete Commit

---
+++ CalculateTrajectoryPoints.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CalculateTrajectoryPoints
+{
+	/// This script is based on the physics trajectory equation
+	/// https://www.omnicalculator.com/physics/trajectory-projectile-motion
+	/// httphttps://en.wikipedia.org/wiki/Projectile_motions://en.wikipedia.org/wiki/Projectile_motion
+	
+	public CalculateTrajectoryPoints(float gravitationPower)
+	{
+		this.gravitationPower = gravitationPower;
+	}
+	
+	private float gravitationPower;
+
+	internal Vector2 CalculatePosition(float time, float distanceToGround, Vector2 velocity)
+	{
+		Vector2 position;
+		position.x = velocity.x * time;
+		position.y = distanceToGround + velocity.y * time - gravitationPower * (time * time) / 2;
+		
+		return position;
+	}
+	
+	internal float TimeToHitTheGround(float distanceToGround, Vector2 velocity)
+	{
+		float a = gravitationPower / 2;
+		float b = -velocity.y;
+		float c = -distanceToGround;
+		float delta = CalculateDelta(b, a, c);
+		Vector2 functionZeros = CalculateFunctionZeros(delta, a, b);
+
+		float time1 = functionZeros.x;
+		float time2 = functionZeros.y;
+		if (time1 > time2)
+		{
+			return time1;
+		}
+		return time2;
+	}
+
+	private static float CalculateDelta(float b, float a, float c)
+	{
+		return (b*b) - 4*a*c;
+	}
+
+	private static Vector2 CalculateFunctionZeros(float delta, float a, float b)
+	{
+		float part1 = -b / (2*a);
+		float part2 = Mathf.Sqrt(delta) / (2*a);
+		
+		float x1 = part1 - part2;
+		float x2 = part1 + part2;
+		
+		return new Vector2(x1, x2);
+	} 
+	
+
+}
---
+++ SetTrajectoryPoints.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class SetTrajectoryPoints : MonoBehaviour
+{
+    public Vector2 velocity;
+    
+    [SerializeField] GameObject trajectoryPointGameObject;
+    [SerializeField] Transform trajectoryPointsParent;
+    [SerializeField] int numberOfTrajectoryPoints = 12;
+    [SerializeField] float maxWorldXCoordinate;
+    [SerializeField] float groundHeight;
+
+    private Vector2 previousVelocity;
+    private GameObject[] trajectoryPoints;
+    private CalculateTrajectoryPoints calculateTrajectory;
+    
+    private void Start()
+    {
+        calculateTrajectory = new CalculateTrajectoryPoints(-Physics2D.gravity.y);
+        InstantiateTrajectoryPoints();
+    }
+    private void InstantiateTrajectoryPoints()
+    {
+        trajectoryPoints = new GameObject[numberOfTrajectoryPoints];
+        for (int i = 0; i < numberOfTrajectoryPoints; i++)
+        {
+            GameObject trajectoryPoint = Instantiate(trajectoryPointGameObject, trajectoryPointsParent);
+            trajectoryPoints[i] = trajectoryPoint;
+        }
+    }
+	
+    private void Update()
+    {
+        if (previousVelocity != velocity)
+        {
+            SetPoints();
+            previousVelocity = velocity;
+        }
+    }
+
+    private void SetPoints()
+    {
+        float distanceToTheGround = transform.position.y - groundHeight;
+        float totalFlightTime = calculateTrajectory.TimeToHitTheGround(distanceToTheGround, velocity);
+        float timeStep = totalFlightTime / (numberOfTrajectoryPoints - 1);
+        float time = 0f;
+        foreach (var trajectoryPoint in trajectoryPoints)
+        {
+            Vector2 offset = transform.position - (Vector3.up * distanceToTheGround);
+            Vector2 position = calculateTrajectory.CalculatePosition(time, distanceToTheGround, velocity) + offset;
+            trajectoryPoint.transform.position = position;
+            if (PositionIsOverMaxXCoordinate(position))
+            {
+                if (Events.LaunchObject != null) Events.LaunchObject();
+            }
+            
+            time += timeStep;
+        }
+    }
+
+    private bool PositionIsOverMaxXCoordinate(Vector2 position)
+    {
+        return position.x > maxWorldXCoordinate;
+    }
+}
